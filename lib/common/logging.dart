d168fe23907661fa1085c6a139f3b9da340fc748
Ravi Teja Gudapati <tejainece@gmail.com>
2018-07-29 15:27:39 +0200

    Reorganised req_resp_connection

---
+++ lib/common/logging.dart
@@ -0,0 +1,3 @@
+import 'package:logging/logging.dart';
+
+final logger = new Logger("MySqlConnection");
--- lib/results/row.dart
+++ lib/results/row.dart
@@ -1,6 +1,10 @@
 library sqljocky.row;
 
+import 'dart:async';
 import 'dart:collection';
+
+import 'field.dart';
+import 'results_impl.dart';
 
 /**
  * A row of data. Fields can be retrieved by index, or by name.
@@ -9,3 +13,22 @@ import 'dart:collection';
  * identifiers, and which aren't part of the List object, can be used.
  */
 abstract class Row extends ListBase<dynamic> {}
+
+class Results extends IterableBase<Row> {
+  final int insertId;
+  final int affectedRows;
+  final List<Field> fields;
+  final List<Row> _rows;
+
+  Results(this._rows, this.fields, this.insertId, this.affectedRows);
+
+  static Future<Results> read(ResultsStream r) async {
+    var rows = await r.toList();
+    return new Results(rows, r.fields, r.insertId, r.affectedRows);
+  }
+
+  @override
+  Iterator<Row> get iterator {
+    return _rows.iterator;
+  }
+}
